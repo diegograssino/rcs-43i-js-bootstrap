6a1f761955cecd9a739a87281662301d461c56af
Diego Grassino <diegograssino@gmail.com>
2023-05-24 18:59:45 -0300

    Agrego sort

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,29 @@ const products = mockData;
 const catalogue =
   document.getElementById('catalogue');
 
+const asc = document.getElementById('asc');
+asc.addEventListener('click', () => {
+  asc.disabled = true;
+  asc.className = 'btn btn-success';
+  desc.disabled = false;
+  desc.className = 'btn btn-outline-success';
+  products.sort((a, b) => a.price - b.price);
+  renderCards(products, catalogue);
+});
+
+const desc = document.getElementById('desc');
+desc.addEventListener('click', () => {
+  desc.disabled = true;
+  desc.className = 'btn btn-success';
+  asc.disabled = false;
+  asc.className = 'btn btn-outline-success';
+  products.sort((a, b) => b.price - a.price);
+  renderCards(products, catalogue);
+});
+
 // 3- Trabajo con la lógica
 // Renderizo las card
+products.sort((a, b) => a.price - b.price);
 renderCards(products, catalogue);
 
 // 4- Revisar si hay que "hacer limpieza"
